bffbb318abe672575198bee2302c0ca7ec639777
Tim Kirves <tim.kirves@gmail.com>
2020-04-19 02:40:36 +0300

    layout, add todo added

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
-import { TodoService } from 'src/app/services/todo.service';
-
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 
 import { Todo } from 'src/app/models/Todo';
 
@@ -10,33 +9,33 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-
   @Input() todo: Todo;
   @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService:TodoService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
-  //Set Dynamic Classes
-
+  // Set Dynamic Classes
   setClasses() {
     let classes = {
       todo: true,
       'is-complete': this.todo.completed
     }
+
     return classes;
   }
 
   onToggle(todo) {
-    //Toggle in UI
+    // Toggle in UI
     todo.completed = !todo.completed;
-    //Toggle on server
+    // Toggle on server
     this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
-
   }
+
   onDelete(todo) {
     this.deleteTodo.emit(todo);
   }
+
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoService } from 'src/app/services/todo.service'
+import { TodoService } from '../../services/todo.service';
 
-import { Todo } from 'src/app/models/Todo';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -9,18 +9,27 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-  todos: Todo[];
+  todos:Todo[];
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
-    this.todoService.getTodos().subscribe(todos => { this.todos = todos });
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
-  deleteTodo(todo: Todo){
-    //Remove From UI
-    this.todos =this.todos.filter(t=>t.id!==todo.id);
-    //Remove from server
+  deleteTodo(todo:Todo) {
+    // Remove From UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // Remove from server
     this.todoService.deleteTodo(todo).subscribe();
   }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
+
 }
